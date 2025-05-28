a1a2d18000cb9ad3c63f76ac19306e5734e57d0f
zhangxh1023 <zhangxh1023@gmail.com>
2025-05-28 14:52:18 +0800

    fix: ollama embedding provider issue (#195)

--- packages/core/src/utils/embed.ts
+++ packages/core/src/utils/embed.ts
@@ -1,4 +1,8 @@
+import type { EmbedManyResult } from '@xsai/embed'
+
+import { EmbeddingProvider } from '@tg-search/common'
 import { useConfig } from '@tg-search/common/composable'
+import { createOllama } from '@xsai-ext/providers-local'
 import { embedMany } from '@xsai/embed'
 
 import { Ok } from './monad'
@@ -6,12 +10,27 @@ import { Ok } from './monad'
 export async function embedContents(contents: string[]) {
   const embeddingConfig = useConfig().api.embedding
 
-  const embeddings = await embedMany({
-    apiKey: embeddingConfig.apiKey,
-    baseURL: embeddingConfig.apiBase || '',
-    input: contents,
-    model: embeddingConfig.model,
-  })
+  let embeddings: EmbedManyResult
+  switch (embeddingConfig.provider) {
+    case EmbeddingProvider.OPENAI:
+      embeddings = await embedMany({
+        apiKey: embeddingConfig.apiKey,
+        baseURL: embeddingConfig.apiBase || '',
+        input: contents,
+        model: embeddingConfig.model,
+      })
+      break
+    case EmbeddingProvider.OLLAMA: {
+      const ollama = createOllama()
+      embeddings = await embedMany({
+        ...ollama.chat(embeddingConfig.model),
+        input: contents,
+      })
+    }
+      break
+    default:
+      throw new Error(`Unsupported embedding model: ${embeddingConfig.provider}`)
+  }
 
   return Ok({
     ...embeddings,
