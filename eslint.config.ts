38f979c82611aded9dccb5105542308b2142e411
RainbowBird <rbxin2003@outlook.com>
2025-07-12 14:24:26 +0800

    feat(package): eslint config package (#293)

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,34 +1,10 @@
-import antfu from '@antfu/eslint-config'
+import config from '@tg-search/eslint-config'
 
-export default await antfu({
+export default await config({
   vue: true,
   unocss: true,
   ignores: [
     'cspell.config.yaml',
     '**/drizzle/**/*.json',
   ],
-  rules: {
-    'import/order': 'off',
-    'antfu/import-dedupe': 'error',
-    'style/padding-line-between-statements': 'error',
-    'perfectionist/sort-imports': [
-      'error',
-      {
-        groups: [
-          'type-builtin',
-          'type-import',
-          'type-internal',
-          ['type-parent', 'type-sibling', 'type-index'],
-          'value-builtin',
-          'value-external',
-          'value-internal',
-          ['value-parent', 'value-sibling', 'value-index'],
-          ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
-          'side-effect',
-          'style',
-        ],
-        newlinesBetween: 'always',
-      },
-    ],
-  },
 })
